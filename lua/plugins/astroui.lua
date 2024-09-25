b79fa37aa57806d56aca4ce7ba295f4f3a618914
runharu0662 <haruta141@icloud.com>
2024-09-26 04:17:42 +0900

    background-transparency-reviced3

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -13,10 +13,10 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+       Normal = { bg = "NONE" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-       Normal = { bg = "#000000" },
+       Normal = { bg = "NONE" },
       },
     },
     -- Icons can be configured throughout the interface
