3f7b1db4a086636e7a19ad51039ccf17d4cc372b
runharu0662 <haruta141@icloud.com>
2024-09-26 05:16:39 +0900

    auto

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -27,7 +27,6 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
