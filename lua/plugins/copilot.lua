4d5045d3b5735fda0b694648f36aec2dd60d7619
runharu0662 <haruta141@icloud.com>
2024-09-27 04:25:11 +0900

    keymaps-for-copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,9 +2,7 @@ return {
   "github/copilot.vim",
   lazy = false,  -- プラグインを常にロード
   config = function()
-    -- Copilot のデフォルト設定をカスタマイズ
-    vim.g.copilot_enabled = 0 --  デフォルトで無効にする
-    vim.g.copilot_no_tab_map =  false -- タブキーを無効にする
+    vim.g.copilot_no_tab_map =  true -- タブキーを無効にする
 
     vim.api.nvim_create_user_command('ToggleCopilot', function()
       if vim.g.copilot_enabled == 1 then
@@ -18,8 +16,15 @@ return {
       end
     end, {})
 
+    local keymap = vim.keymap.set
+
+    keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true, replace_keycodes = false })
+    keymap("i", "<C-a>j", "<Plug>(copilot-next)")
+    keymap("i", "<C-a>k", "<Plug>(copilot-previous)")
+    keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
+    keymap("i", "<C-s>", "<Plug>(copilot-suggest)")
+
     -- Copilot のトグルをキーに割り当てる (例: <leader>cp)
     vim.api.nvim_set_keymap('n', '<leader>cp', ':ToggleCopilot<CR>', { noremap = true, silent = true })
   end
 }
-
