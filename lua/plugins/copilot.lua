dde44641eb0caac26c4cda997885a0bb05d2714f
runharu0662 <haruta141@icloud.com>
2024-09-27 04:41:26 +0900

    auto

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -19,8 +19,8 @@ return {
     local keymap = vim.keymap.set
 
     keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true, replace_keycodes = false })
-    keymap("i", "<C-w>j", "<Plug>(copilot-next)")
-    keymap("i", "<C-w>k", "<Plug>(copilot-previous)")
+    keymap("i", "<C-j>", "<Plug>(copilot-next)")
+    keymap("i", "<C-k>", "<Plug>(copilot-previous)")
     keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
     keymap("i", "<C-s>", "<Plug>(copilot-suggest)")
 
