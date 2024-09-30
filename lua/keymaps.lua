f2158d87eb32d96b80765474691178600659ca49
runharu0662 <haruta141@icloud.com>
2024-09-30 17:47:31 +0900

    auto

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,19 +1,17 @@
 -- cppファイルの実行
-vim.api.nvim_set_keymap('n', '<Space>I<Space>', ':wa <bar> :QuickRun <stdin.txt<CR>', { noremap = true, silent = true }, desc = "cpp QuickRun") 
+vim.keymap.set('n', '<Space>I<Space>', ':wa <bar> :QuickRun <stdin.txt<CR>', { noremap = true, silent = true, desc = "cpp QuickRun" })
 
 -- テンプレートファイルの挿入コマンド
 vim.api.nvim_create_user_command('InsTemp', function()
-  -- テンプレートファイルの相対パスを指定
-  local current_dir = vim.fn.expand('%:p:h')
+  -- テンプレートファイルのパスを指定
   local template_path = '~/.config/nvim-astro/template/atc.cpp'
 
-
-  -- 現在のバッファにテンプレートファイルの内容を挿入
+  -- テンプレートファイルの内容を現在のバッファに挿入
   vim.cmd('0r ' .. template_path)
 end, {})
 
 -- Space i t のショートカットに割り当てる
-vim.api.nvim_set_keymap('n', '<Space>It', ':InsTemp<CR>', { noremap = true, silent = true }, desc = "cpp Template" )
+vim.keymap.set('n', '<Space>It', ':InsTemp<CR>', { noremap = true, silent = true, desc = "Insert cpp Template" })
 
 -- jkでEscを実行する（挿入モード）
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
