ebe1cb6738e7268c93f0612e4b6d9907d79e4e2d
runharu0662 <haruta141@icloud.com>
2024-09-30 17:22:09 +0900

    hop-reviced-and-git

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -6,23 +6,9 @@ return {
 		multi_windows = true,
 	},
 	keys = {
-		{ "<leader>w", "<cmd>HopWord<CR>", mode = "n", desc = "Hop Word" },
-		{ "<leader>l", "<cmd>HopLine<CR>", mode = "n", desc = "Hop Line" },
-		{ "<leader>c", "<cmp>HopChar1<CR>", mode = "n", desc = "Hop Char" },
-		{ "<leader>p", "<cmd>HopPattern<CR>", mode = "n", desc = "Hop Pattern" },
+		{ "<leader>W", "<cmd>HopWord<CR>", mode = "n", desc = "Hop Word" },
+		{ "<leader>L", "<cmd>HopLine<CR>", mode = "n", desc = "Hop Line" },
 		{ "f", "<cmd>HopChar1CurrentLineAC<CR>", mode = { "n", "v", "o" }, desc = "Hop Char in Line (After Cursor)" },
 		{ "F", "<cmd>HopChar1CurrentLineBC<CR>", mode = { "n", "v", "o" }, desc = "Hop Char in Line (Before Cursor)" },
-		{
-			"t",
-			"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>",
-			mode = { "n", "v", "o" },
-			desc = "Hop Before Char in Line (After Cursor)",
-		},
-		{
-			"T",
-			"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>",
-			mode = { "n", "v", "o" },
-			desc = "Hop After Char in Line (Before Cursor)",
-		},
 	},
 }
