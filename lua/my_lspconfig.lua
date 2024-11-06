faad13757ffad4932a1b17756674c47ef57b5a69
runharu0662 <haruta141@icloud.com>
2024-11-06 18:32:59 +0900

    auto

--- lua/my_lspconfig.lua
+++ lua/my_lspconfig.lua
@@ -1,13 +1,3 @@
-require("lspconfig").ccls.setup {
-    init_options = {
-        cache = {
-            directory = "/tmp/ccls",
-        },
-        completion = {
-            filterAndSort = false,
-        },
-    },
-}
 require("lspconfig").clangd.setup {
     cmd = { "clangd", "--std=c++20" },
     settings = {},
