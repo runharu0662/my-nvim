1c86177025d25cc383f9118ca765bcf564cc384f
runharu0662 <haruta141@icloud.com>
2024-11-07 19:05:28 +0900

    auto

--- lua/my_lspconfig.lua
+++
@@ -1,8 +0,0 @@
-require("mason-lspconfig").setup_handlers {
-    function(server_name) require("lspconfig")[server_name].setup {} end,
-    ["clangd"] = function()
-        require("lspconfig").clangd.setup {
-            cmd = { "clangd", "--std=c++20" },
-        }
-    end,
-}
