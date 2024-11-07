9ca1d3104cfecc81ac93f206e963f68725ffb971
runharu0662 <haruta141@icloud.com>
2024-11-07 20:32:01 +0900

    deleteIt

--- lua/my_lspconfig.lua
+++
@@ -1,8 +0,0 @@
-require("lspconfig").clangd.setup {
-    cmd = { "clangd", "--clang-tidy", "--header-insertion=never", "--completion-style=detailed" },
-    settings = {
-        diagnostics = {
-            suppress = { "unused-includes" },
-        },
-    },
-}
