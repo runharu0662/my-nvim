21c8abe77b74c111514b467b1c8c7be84c533247
runharu0662 <haruta141@icloud.com>
2024-11-07 18:57:45 +0900

    lspclagd

--- lua/my_lspconfig.lua
+++ lua/my_lspconfig.lua
@@ -1,3 +1,8 @@
-require("lspconfig").clangd.setup {
-    cmd = { "clangd", "--std=c++20" },
+require("mason-lspconfig").setup_handlers {
+    function(server_name) require("lspconfig")[server_name].setup {} end,
+    ["clangd"] = function()
+        require("lspconfig").clangd.setup {
+            cmd = { "clangd", "--std=c++20" },
+        }
+    end,
 }
