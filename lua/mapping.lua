edfaebbc1699f756a0652ff15ec53cd44e1f7e7c
runharu0662 <haruta141@icloud.com>
2025-04-14 17:42:19 +0900

    remove_jk_esc

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -4,18 +4,18 @@ vim.keymap.set("n", "<Space>i<Space>", ":wa <bar> :QuickRun <stdin.txt<CR>", { n
 
 -- テンプレートファイルの挿入コマンド
 vim.api.nvim_create_user_command("InsTemp", function()
-    -- テンプレートファイルのパスを指定
-    local template_path = "~/.config/nvim/template/atc.cpp"
+  -- テンプレートファイルのパスを指定
+  local template_path = "~/.config/nvim/template/atc.cpp"
 
-    -- テンプレートファイルの内容を現在のバッファに挿入
-    vim.cmd("0r " .. template_path)
+  -- テンプレートファイルの内容を現在のバッファに挿入
+  vim.cmd("0r " .. template_path)
 end, {})
 
 -- Space i t のショートカットに割り当てる
 vim.keymap.set("n", "<Space>it", ":InsTemp<CR>", { noremap = true, silent = true, desc = "Insert cpp Template" })
 
 -- jkでEscを実行する（挿入モード）
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+-- vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 -- 行の折り返し
 vim.opt.wrap = true
