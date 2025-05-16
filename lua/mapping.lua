0f4a89ce704f9136d76668926df35e200516b1f0
runharu0662 <harutastudy0662@gmail.com>
2025-05-16 19:07:28 +0900

    revice_path_to_template

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,16 +1,11 @@
 -- cppファイルの実行
 vim.keymap.set("n", "<leader>i", "<Nop>", { noremap = true, silent = true, desc = "about cpp" })
-vim.keymap.set(
-    "n",
-    "<Space>i<Space>",
-    ":wa <bar> :QuickRun <stdin.txt<CR>",
-    { noremap = true, silent = true, desc = "cpp QuickRun" }
-)
+vim.keymap.set("n", "<Space>i<Space>", ":wa <bar> :QuickRun <stdin.txt<CR>", { noremap = true, silent = true, desc = "cpp QuickRun" })
 
 -- テンプレートファイルの挿入コマンド
 vim.api.nvim_create_user_command("InsTemp", function()
     -- テンプレートファイルのパスを指定
-    local template_path = "~/.config/nvim-astro/template/atc.cpp"
+    local template_path = "~/.config/nvim/template/atc.cpp"
 
     -- テンプレートファイルの内容を現在のバッファに挿入
     vim.cmd("0r " .. template_path)
@@ -27,17 +22,7 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 
 -- Normal mode (n)
-vim.keymap.set(
-    "n",
-    "<A-i>",
-    "<cmd>ToggleTerm direction=float<CR>",
-    { noremap = true, silent = true, desc = "Toggle floating terminal" }
-)
+vim.keymap.set("n", "<A-i>", "<cmd>ToggleTerm direction=float<CR>", { noremap = true, silent = true, desc = "Toggle floating terminal" })
 
 -- Terminal mode (t)
-vim.keymap.set(
-    "t",
-    "<A-i>",
-    "<cmd>ToggleTerm direction=float<CR>",
-    { noremap = true, silent = true, desc = "Toggle floating terminal" }
-)
+vim.keymap.set("t", "<A-i>", "<cmd>ToggleTerm direction=float<CR>", { noremap = true, silent = true, desc = "Toggle floating terminal" })
