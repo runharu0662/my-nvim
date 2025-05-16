ff1a8d5d4b37c559e25dd4eadedafa41c374045d
runharu0662 <harutastudy0662@gmail.com>
2025-05-16 21:38:42 +0900

    clangd_lsp_in_init.lua

--- init.lua
+++ init.lua
@@ -15,6 +15,13 @@ if not pcall(require, "lazy") then
     vim.cmd.quit()
 end
 
+-- clang for arch
+local lspconfig = require "lspconfig"
+lspconfig.clangd.setup {
+    cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+    init_options = { fallbackFlags = { "-std=c++20" } },
+}
+
 require "lazy_setup"
 require "polish"
 require "mapping"
